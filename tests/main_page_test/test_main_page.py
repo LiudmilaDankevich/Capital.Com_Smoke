f6fd23b9a2cb7e7fe3e1084cd316b0ae51eba01a
Liudmial_Dankevich251015 <79874283+LiudmilaDankevich@users.noreply.github.com>
2022-10-27 12:15:23 +0300

    open_base_page

---
+++ tests/main_page_test/test_main_page.py
@@ -0,0 +1,7 @@
+from pages.main_page import MainPage
+
+
+def test_click_sing_in_button(browser):
+    main_page = MainPage(browser)
+    main_page.open_base_page()
+    # main_page.open_login_page()
