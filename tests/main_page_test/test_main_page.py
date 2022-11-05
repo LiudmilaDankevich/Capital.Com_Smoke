86c177c743564517f04852d64f3f1041c2734283
Liudmial_Dankevich251015 <79874283+LiudmilaDankevich@users.noreply.github.com>
2022-11-06 01:52:56 +0300

    Login_page

--- tests/main_page_test/test_main_page.py
+++ tests/main_page_test/test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 from time import sleep
 
 
@@ -8,4 +9,8 @@ def test_main_page_show(browser):
     sleep(10)
     main_page.main_page_text_is_present()
     main_page.should_be_trade_button_mp1()
+    # login_page = LoginPage(browser)
+    # sleep(2)
+    # login_page.open_login_page()
+    # sleep(2)
 
